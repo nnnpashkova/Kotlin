688327831631b8bf820229e97dc4d0f5857f589d
Nata <nata.mir.99@mail.ru>
2023-05-11 18:20:20 +0300

    KS-1-2 variable declaration,printed number of employees

--- src/main/kotlin/lesson1/lesson_1_task_2.kt
+++ src/main/kotlin/lesson1/lesson_1_task_2.kt
@@ -1,11 +1,12 @@
 package lesson1
 
-fun main() {
-    val numberOrder: Int = 75
-    val textThanksOrder: String = "Спасибо за покупку"
-    var numberWorker:Int = 2000
-    //println(numberWorker)
-    numberWorker = 1999
-    println(numberWorker)
-
+class lesson_1_task_2 {
+    fun main() {
+        val numberOrder: Int = 75
+        val textThanksOrder: String = "Спасибо за покупку"
+        var numberWorker:Int = 2000
+        //println(numberWorker)
+        numberWorker = 1999
+        println(numberWorker)
+    }
 }
