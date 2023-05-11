d96f434e9138a535b165badcd4d007f7d48cbea1
Nata <nata.mir.99@mail.ru>
2023-05-11 22:20:09 +0300

    KS-1-3 implemented task 3 for lesson 1

---
+++ src/main/kotlin/lesson1/lesson_1_task_3.kt
@@ -0,0 +1,11 @@
+package lesson1
+
+fun main(){
+    val year = 1961
+    var hour = 9
+    var minute = 7
+    println("$year \n 0$hour \n 0$minute")
+    hour = 10
+    minute = 55
+    println("$hour:$minute")
+}
