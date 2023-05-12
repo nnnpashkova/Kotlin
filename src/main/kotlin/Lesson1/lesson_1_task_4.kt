0b62c9514c4ef258b148f12a19ed17ebc87e7215
Nata <nata.mir.99@mail.ru>
2023-05-12 19:11:38 +0300

    KS-1-4 implemented task 4 for lesson 1

---
+++ src/main/kotlin/Lesson1/lesson_1_task_4.kt
@@ -0,0 +1,11 @@
+package Lesson1
+fun main(){
+    val length:Long = 40_868_600_000
+    val age:Byte = 27
+    val partOfDay:Byte = 0.075
+    val seconds:Short = 6480
+    val partOfYear:Byte = 2.0547945205479453E-4
+    val apogee:Int = 327000
+
+}
+
