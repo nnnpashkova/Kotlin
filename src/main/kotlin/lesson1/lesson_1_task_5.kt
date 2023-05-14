17985f6b5438a4ee147bc3af28c5f0cdb15944c2
Nata <nata.mir.99@mail.ru>
2023-05-14 21:56:55 +0300

    KS-1-5 fix

--- src/main/kotlin/lesson1/lesson_1_task_5.kt
+++ src/main/kotlin/lesson1/lesson_1_task_5.kt
@@ -2,6 +2,12 @@ package lesson1
 
 fun main(){
     val seconds = 6480
-    val secondsInMinute = "108:00"
-    println(secondsInMinute)
+    val secondsInMinute= 60
+    val minute = seconds/secondsInMinute
+    val secondsRemain = seconds % secondsInMinute
+    if(secondsRemain >= 10) {
+        println("$minute:$secondsRemain")
+    } else {
+        println("$minute:0$secondsRemain")
+    }
 }
