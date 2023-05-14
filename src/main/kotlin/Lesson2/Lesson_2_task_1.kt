9d80adf49b9efffc95c2667fe7ec6c968eff3226
Nata <nata.mir.99@mail.ru>
2023-05-14 22:19:26 +0300

    KS-2-1 implemented task 1 for lesson 2

---
+++ src/main/kotlin/Lesson2/Lesson_2_task_1.kt
@@ -0,0 +1,13 @@
+package Lesson2
+
+fun main(){
+    val englishStudent:Float = 4F
+    val x1 = 3
+    val x2 = 4
+    val x3 = 3
+    val x4 = 5
+    val sumOfGrades = x1+x2+x3+x4
+    val average = sumOfGrades/anglStudent
+    println(average)
+
+}
