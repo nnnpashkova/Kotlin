d11ee6dba0d0d9aa48981ed88e25646cdcd76d57
Nata <nata.mir.99@mail.ru>
2023-05-16 20:21:14 +0300

    KS-2-3 implemented task 3 for lesson 2 - fix indentation

--- src/main/kotlin/lesson2/lesson_2_task_3.kt
+++ src/main/kotlin/lesson2/lesson_2_task_3.kt
@@ -3,10 +3,10 @@ fun main(){
     val theTrainLeftInHours = 9
     val theTrainLeftInMinutes = 39
     val travelTimeMinute = 457
-    val hourOfMinutes= 60
-    val theTrainHasLeft = (theTrainLeftInHours*hourOfMinutes)+theTrainLeftInMinutes
-    val travelTimeInMinutesTotal = travelTimeMinute+theTrainHasLeft
-    val travelTimeInHours = travelTimeInMinutesTotal/hourOfMinutes
-    val travelTimeInMinutes = travelTimeInMinutesTotal%hourOfMinutes
+    val hourOfMinutes = 60
+    val theTrainHasLeft = theTrainLeftInHours*hourOfMinutes + theTrainLeftInMinutes
+    val travelTimeInMinutesTotal = travelTimeMinute + theTrainHasLeft
+    val travelTimeInHours = travelTimeInMinutesTotal / hourOfMinutes
+    val travelTimeInMinutes = travelTimeInMinutesTotal % hourOfMinutes
     println("$travelTimeInHours:$travelTimeInMinutes")
 }
