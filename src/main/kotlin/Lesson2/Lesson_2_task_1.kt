6dba7f87afd342a42ee3fbc78b451fa784ce6a04
Nata <nata.mir.99@mail.ru>
2023-05-16 20:13:24 +0300

    KS-2-3 implemented task 3 for lesson 2 - fix indentation

--- src/main/kotlin/Lesson2/Lesson_2_task_1.kt
+++ src/main/kotlin/Lesson2/Lesson_2_task_1.kt
@@ -1,13 +1,12 @@
 package Lesson2
 
 fun main(){
-    val englishStudent:Float = 4F
+    val englishStudent: Float = 4F
     val x1 = 3
     val x2 = 4
     val x3 = 3
     val x4 = 5
-    val sumOfGrades = x1+x2+x3+x4
-    val average = sumOfGrades/englishStudent
+    val sumOfGrades = x1 + x2 + x3 + x4
+    val average = sumOfGrades / englishStudent
     println(average)
-
 }
