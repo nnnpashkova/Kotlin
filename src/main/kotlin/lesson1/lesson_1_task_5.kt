9db45dc6775055e8fd592f952832032f925cd809
Nata <nata.mir.99@mail.ru>
2023-05-17 21:30:53 +0300

    KS-1-5 implemented task 5 for lesson 1 - fix (String.format)

--- src/main/kotlin/lesson1/lesson_1_task_5.kt
+++ src/main/kotlin/lesson1/lesson_1_task_5.kt
@@ -1,13 +1,9 @@
 package lesson1
 
-fun main(){
+fun main() {
     val seconds = 6480
-    val secondsInMinute= 60
-    val minute = seconds/secondsInMinute
+    val secondsInMinute = 60
+    val minute = seconds / secondsInMinute
     val secondsRemain = seconds % secondsInMinute
-    if(secondsRemain >= 10) {
-        println("$minute:$secondsRemain")
-    } else {
-        println("$minute:0$secondsRemain")
-    }
+    println(String.format("%02d:%02d", minute, secondsRemain))
 }
