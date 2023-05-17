a79b713c85dc12e402ff1b89e1c37c4c6f8be189
Nata <nata.mir.99@mail.ru>
2023-05-17 21:50:53 +0300

    KS-2-4 implemented task 4 for lesson 2

--- src/main/kotlin/Lesson2/lesson_2_task_4.kt
+++ src/main/kotlin/Lesson2/lesson_2_task_4.kt
@@ -1,7 +1,13 @@
 package Lesson2
 
 fun main(){
-    val buff = 20
+    val buffPercent = 0.2
     val crystalline = 7
     val iron = 11
+    val crystallineBuff = (crystalline * buffPercent).toInt()
+    val crystallineWithBuff = crystallineBuff + crystalline
+    println(crystallineWithBuff)
+    val ironBuff = (iron * buffPercent).toInt()
+    val ironWithBuff = ironBuff + iron
+    println(ironWithBuff)
 }
