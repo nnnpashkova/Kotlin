a4a62fa6dfeab621aa7db36083f98ef24eb46bbe
Nata <nata.mir.99@mail.ru>
2023-05-18 19:19:31 +0300

    KS-3-2 implemented task 3 for lesson 2 - fix

--- src/main/kotlin/lesson3/lesson_3_task_2.kt
+++ src/main/kotlin/lesson3/lesson_3_task_2.kt
@@ -6,7 +6,9 @@ fun main(){
     val middleName = "Сергеевна"
     val age = 20
     var dataOnTheChangeOfSurname = "$surnameMaiden $name $middleName, $age"
+
     println(dataOnTheChangeOfSurname)
+
     val surnameModified = "Сидорова"
     val ageChangeOfSurname = 22
     dataOnTheChangeOfSurname = "$surnameModified $name $middleName, $ageChangeOfSurname"
