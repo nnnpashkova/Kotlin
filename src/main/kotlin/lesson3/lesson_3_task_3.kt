975c84bce4c5f5d4179997442c96d149c73a35c0
Nata <nata.mir.99@mail.ru>
2023-05-19 17:08:59 +0300

    KS-3-3 implemented task 3 for lesson 3 - fix

--- src/main/kotlin/lesson3/lesson_3_task_3.kt
+++ src/main/kotlin/lesson3/lesson_3_task_3.kt
@@ -2,14 +2,14 @@ package lesson3
 
 fun main(){
    val multiplicationTableNumber = 9
-    println("9 × 1 = ${multiplicationTableNumber * 1} \n" +
-            "9 × 2 = ${multiplicationTableNumber * 2} \n" +
-            "9 × 3 = ${multiplicationTableNumber * 3} \n" +
-            "9 × 4 = ${multiplicationTableNumber * 4} \n" +
-            "9 × 5 = ${multiplicationTableNumber * 5} \n" +
-            "9 × 6 = ${multiplicationTableNumber * 6} \n" +
-            "9 × 7 = ${multiplicationTableNumber * 7} \n" +
-            "9 × 8 = ${multiplicationTableNumber * 8} \n" +
-            "9 × 9 = ${multiplicationTableNumber * 9}")
+    println("$multiplicationTableNumber × 1 = ${multiplicationTableNumber * 1} \n" +
+            "$multiplicationTableNumber × 2 = ${multiplicationTableNumber * 2} \n" +
+            "$multiplicationTableNumber × 3 = ${multiplicationTableNumber * 3} \n" +
+            "$multiplicationTableNumber × 4 = ${multiplicationTableNumber * 4} \n" +
+            "$multiplicationTableNumber × 5 = ${multiplicationTableNumber * 5} \n" +
+            "$multiplicationTableNumber × 6 = ${multiplicationTableNumber * 6} \n" +
+            "$multiplicationTableNumber × 7 = ${multiplicationTableNumber * 7} \n" +
+            "$multiplicationTableNumber × 8 = ${multiplicationTableNumber * 8} \n" +
+            "$multiplicationTableNumber × 9 = ${multiplicationTableNumber * 9}")
 
 }
