78c889edd0c82f86dd27ac56d3302b94b5e95d60
Nata <nata.mir.99@mail.ru>
2023-05-19 13:00:32 +0300

    KS-5-4 implemented task 4 for lesson 5

---
+++ src/main/kotlin/lesson5/lesson_5_task_4.kt
@@ -0,0 +1,21 @@
+package lesson5
+
+fun main(){
+    println("Bведите логин:")
+    val loginInput = readln()
+    val loginAnswerCorrect = loginInput == LOGIN
+    if (loginAnswerCorrect ){
+        println("Введите введите пароль:")
+    } else {
+        println("Такой аккаунт не зарегистрирован. Зарегистрироваться?")
+    }
+    val passwordInput = readln()
+    if (passwordInput == PASSWORD) {
+        println("Авторизация прошла успешно. Добро пожаловать!")
+    } else {
+        println("Ошибка авторизации")
+    }
+}
+const val LOGIN = "Batman"
+const val PASSWORD = "password"
+
