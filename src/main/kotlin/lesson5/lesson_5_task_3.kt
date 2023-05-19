09df8820aeaebf1f793163abc6239730abd06318
Nata <nata.mir.99@mail.ru>
2023-05-19 12:21:39 +0300

    KS-5-3 implemented task 3 for lesson 5

---
+++ src/main/kotlin/lesson5/lesson_5_task_3.kt
@@ -0,0 +1,27 @@
+package lesson5
+
+fun main() {
+    val firstNumber = 8
+    val secondNumber = 12
+
+    println("Какого числа отмечается междунородный женский день? ")
+    val userFirstNumber = readln().toInt()
+    val firstAnswerCorrect = firstNumber == userFirstNumber
+    if (firstAnswerCorrect) {
+        println("Вы молодец")
+    }
+
+    println("Какого числа отмечается День космонавтики? ")
+    val userSecondNumber = readln().toInt()
+    val secondAnswerCorrect = secondNumber == userSecondNumber
+    if (secondAnswerCorrect) {
+        println("Супер")
+    }
+    if (firstAnswerCorrect && secondAnswerCorrect) {
+        println("Поздравляем! Вы выиграли главный приз!")
+    } else if (firstAnswerCorrect || secondAnswerCorrect) {
+        println("Вы выиграли утешительный приз!")
+    } else {
+       println("Неудача! Крутите барабан!")
+    }
+}
