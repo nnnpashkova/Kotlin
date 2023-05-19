82541bf3a0cf35911399514d386ff91491890d2f
Nata <nata.mir.99@mail.ru>
2023-05-19 17:03:19 +0300

    KS-4-3 implemented task 3 for lesson 4 - fix variable names

--- src/main/kotlin/lesson4/lesson_4_task_3.kt
+++ src/main/kotlin/lesson4/lesson_4_task_3.kt
@@ -1,14 +1,14 @@
 package lesson4
 
 fun main(){
-    val weatherToday = "sunny"
-    val awningToday = "open"
+    val isSunny = true
+    val isAwningOpen = true
     val humidityToday = 20
     val seasonToday = "winter"
-    val conditionsWeatherToday = weatherToday == WEATHER && awningToday == AWNING && humidityToday == HUMIDITY && seasonToday != SEASON
+    val conditionsWeatherToday = isSunny == SUNNY_WEATHER && isAwningOpen == AWNING_OPEN && humidityToday == HUMIDITY && seasonToday != SEASON
     println("Благоприятные ли условия сейчас для роста бобовых? $conditionsWeatherToday")
 }
-const val WEATHER = "sunny"
-const val AWNING = "open"
+const val SUNNY_WEATHER = true
+const val AWNING_OPEN = true
 const val HUMIDITY = 20
 const val SEASON = "winter"
