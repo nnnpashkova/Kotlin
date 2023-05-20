33ad699f2cf7e3714ea6988281f69f8c97d7313b
Nata <nata.mir.99@mail.ru>
2023-05-20 11:09:02 +0300

    KS-3-4 implemented task 4 for lesson 3 - fix

--- src/main/kotlin/lesson3/lesson_3_task_4.kt
+++ src/main/kotlin/lesson3/lesson_3_task_4.kt
@@ -4,11 +4,11 @@ fun main(){
     var whenceMove = "E2"
     var whereMove = "E4"
     var moveNumber = "1"
-    var moveResult = ("$whenceMove; $whereMove; $moveNumber")
+    var moveResult = ("[$whenceMove-$whereMove; $moveNumber]")
     println(moveResult)
     whenceMove = "D2"
     whereMove = "D3"
     moveNumber = "2"
-    moveResult = ("$whenceMove; $whereMove; $moveNumber")
+    moveResult = ("[$whenceMove-$whereMove; $moveNumber]")
     println(moveResult)
 }
