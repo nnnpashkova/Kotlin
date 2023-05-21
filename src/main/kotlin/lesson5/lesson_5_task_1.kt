c7d3ae3771b8114b02561633944828ce00d80b98
Nata <nata.mir.99@mail.ru>
2023-05-21 18:47:05 +0300

    KS-5-1 implemented task 1 for lesson 5 - fix

--- src/main/kotlin/lesson5/lesson_5_task_1.kt
+++ src/main/kotlin/lesson5/lesson_5_task_1.kt
@@ -2,16 +2,16 @@ package lesson5
 
 import kotlin.random.Random
 
-fun main(){
+fun main() {
     val firstNumber = (0..100).random()
     val secondNumber = (0..100).random()
     println("$firstNumber + $secondNumber = ")
     var userAge = readln().toInt()
 
-    if (userAge == firstNumber + secondNumber ){
+    if (userAge == firstNumber + secondNumber) {
         println("Добро пожаловать")
     } else {
         println("Доступ запрещен")
     }
-        println("$firstNumber + $secondNumber = ")
+    println("$firstNumber + $secondNumber =")
 }
