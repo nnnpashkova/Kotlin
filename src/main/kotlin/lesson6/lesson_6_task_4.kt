f6c77230772a6af1895cd16aa49b927b0bc5f46b
Nata <nata.mir.99@mail.ru>
2023-05-22 21:29:25 +0300

    KS-6-4 implemented task 4 for lesson 6 - fix

--- src/main/kotlin/lesson6/lesson_6_task_4.kt
+++ src/main/kotlin/lesson6/lesson_6_task_4.kt
@@ -6,14 +6,18 @@ fun main() {
     val correctNumber = 5
     var userEntered = 0
 
-    while (numberOfAttempts < maxNumberOfAttempts || correctNumber == userEntered) {
+    while (numberOfAttempts < maxNumberOfAttempts && correctNumber != userEntered) {
         numberOfAttempts++
         println("Введите число от 1 до 9:")
         val userEntered = readln().toInt()
         if (correctNumber == userEntered) {
             println("Это была великолепная игра!")
+            return
         } else {
             println("Неверно, осталось количество ${maxNumberOfAttempts - numberOfAttempts}")
+        }
+        if (numberOfAttempts == maxNumberOfAttempts) {
+            println("Было загадано число $correctNumber")
         }
     }
     println("Было загадано $correctNumber")
