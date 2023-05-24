187cda33962c3b36e8d7e5e0c7f5e77a7c4768d7
Nata <nata.mir.99@mail.ru>
2023-05-24 22:41:54 +0300

    KS-8-5 implemented task 5 for lesson 8

---
+++ src/main/kotlin/lesson8/lesson_8_task_5.kt
@@ -0,0 +1,13 @@
+package lesson8
+
+fun main() {
+    println("Введите количество ингредиентов:")
+    val quantityIngredients = readln().toInt()
+    val ingredientsArray = arrayOfNulls<String>(quantityIngredients)
+
+    for (i in 0 until  quantityIngredients) {
+       println("Введите ингредиент:")
+        val ingredient = readln()
+        ingredientsArray[i] = ingredient
+    }
+}
