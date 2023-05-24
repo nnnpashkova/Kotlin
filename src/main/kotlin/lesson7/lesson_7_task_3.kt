efae5c74e2ff953bc692860c214bade6832d10cc
Nata <nata.mir.99@mail.ru>
2023-05-24 16:24:27 +0300

    KS-7-3 implemented task 3 for lesson 7 - fix

--- src/main/kotlin/lesson7/lesson_7_task_3.kt
+++ src/main/kotlin/lesson7/lesson_7_task_3.kt
@@ -1,12 +1,10 @@
 package lesson7
 
 fun main() {
-    println ("Введите число:")
+    println("Введите число:")
     val userNumber = readln().toInt()
 
-    for (number in 0.. userNumber){
-       if (number % 2 == 0) {
-           println(number)
-        }
+    for (number in 0..userNumber step 2) {
+        println(number)
     }
 }
