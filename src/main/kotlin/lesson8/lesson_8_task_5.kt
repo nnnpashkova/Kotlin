160dfd074d0f2624f32bc0e84f6e147e39fc1d07
Nata <nata.mir.99@mail.ru>
2023-05-25 22:43:00 +0300

    KS-8-5 implemented task 5 for lesson 8 - fix space

--- src/main/kotlin/lesson8/lesson_8_task_5.kt
+++ src/main/kotlin/lesson8/lesson_8_task_5.kt
@@ -4,7 +4,7 @@ fun main() {
     println("Введите количество ингредиентов:")
     val quantityIngredients = readln().toInt()
     val ingredientsArray = arrayOfNulls<String>(quantityIngredients)
-    for (i in 0..quantityIngredients) {
+    for (i in 0 until quantityIngredients) {
        println("Введите ингредиент:")
         val ingredient = readln()
         ingredientsArray[i] = ingredient
