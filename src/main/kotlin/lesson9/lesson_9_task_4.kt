cf337354dc9e9d5004bab3f58cb566b7d84339b5
Nata <nata.mir.99@mail.ru>
2023-05-26 18:30:25 +0300

    KS-9-4 implemented task 4 for lesson 9 - fix

--- src/main/kotlin/lesson9/lesson_9_task_4.kt
+++ src/main/kotlin/lesson9/lesson_9_task_4.kt
@@ -1,7 +1,7 @@
 package lesson9
 
-fun main(){
-   println("Введите пять ингредиентов блюда:")
+fun main() {
+    println("Введите пять ингредиентов блюда:")
     val userInput = readln()
     val delim = ","
     val splitList = userInput.split(delim)
