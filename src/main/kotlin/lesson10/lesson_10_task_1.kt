997144cb00d3df84bef6d8c4dac578de9911d90e
Nata <nata.mir.99@mail.ru>
2023-05-27 19:11:41 +0300

    KS-10-1 implemented task 1 for lesson 10

--- src/main/kotlin/lesson10/lesson_10_task_1.kt
+++ src/main/kotlin/lesson10/lesson_10_task_1.kt
@@ -2,11 +2,11 @@ package lesson10
 
 fun main() {
     println("Бросает кубики игрок:")
-    val numberPerson = numberGenerator()
+    val numberPerson = sumOfRandomNumbers()
     println(numberPerson)
 
     println("Бросает кубики компьютер:")
-    val numberСomputer = numberGenerator()
+    val numberСomputer = sumOfRandomNumbers()
     println(numberСomputer)
 
     if (numberPerson > numberСomputer) {
@@ -19,7 +19,7 @@ fun main() {
 
 }
 
-fun numberGenerator(): Int {
+fun sumOfRandomNumbers(): Int {
     val numberOne = (0..6).random()
     val numberTwo = (0..6).random()
     return numberOne + numberTwo
