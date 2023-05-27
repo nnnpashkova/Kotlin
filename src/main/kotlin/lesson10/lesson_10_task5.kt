6eb5dbcd8bb9f344ecd35ac9780edc126b03146c
Nata <nata.mir.99@mail.ru>
2023-05-27 23:54:20 +0300

    KS-10-5 implemented task 5 for lesson 10

---
+++ src/main/kotlin/lesson10/lesson_10_task5.kt
@@ -0,0 +1,73 @@
+package lesson10
+
+fun main() {
+    val userRegistrationLogin = userRegistrationLogin()
+    val userRegistrationPassword = userRegistrationPassword()
+    var userAuthorizationLogin = ""
+    var userAuthorizationPassword = ""
+
+    do {
+        userAuthorizationLogin = userAuthorizationLogin()
+        userAuthorizationPassword = userAuthorizationPassword()
+        if (userAuthorizationLogin != userRegistrationLogin || userAuthorizationPassword != userRegistrationPassword) {
+            println("Ошибка, авторизуйтесь заново")
+        }
+    } while (userAuthorizationLogin != userRegistrationLogin || userAuthorizationPassword != userRegistrationPassword)
+
+    var isVerificationCodeCorrect = false
+    while (!isVerificationCodeCorrect) {
+        val generatesCode = generatesAuthorizationCode()
+        println("Ваш код для авторизации: $generatesCode \nВведите код:")
+
+        val userCode = readln().toInt()
+        if (userCode != generatesCode) {
+            println("Неверно. Попробуйте заново")
+        } else {
+            println("Добро пожаловать")
+            isVerificationCodeCorrect = true
+        }
+    }
+}
+
+fun userRegistrationLogin(): String {
+    var userLogin = ""
+    println("Придумайте логин:")
+    while (!isLoginValid(userLogin)) {
+        userLogin = readln()
+        if (!isLoginValid(userLogin)) {
+            println("Логин должен состоять минимум из 4 символов. Ввведите еще раз:")
+        }
+    }
+    return userLogin
+}
+
+fun isLoginValid(login: String): Boolean = login.length >= 4
+
+fun userRegistrationPassword(): String {
+    val length = 10
+    val randomPassword = getRandomString(length)
+    println("Ваш пароль: $randomPassword ")
+    return randomPassword
+}
+
+fun getRandomString(length: Int): String {
+    val charset = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+    return (1..length)
+        .map { charset.random() }
+        .joinToString("")
+}
+
+fun userAuthorizationLogin(): String {
+    println("Введите логин:")
+    return readln()
+}
+
+fun userAuthorizationPassword(): String {
+    println("Введите пароль:")
+    return readln()
+}
+
+fun generatesAuthorizationCode(): Int {
+    val randomСode = (1000..9999).random()
+    return randomСode
+}
