cf5506a1aeaf0f7faaca76fc231371fc9a6f00d5
Nata <nata.mir.99@mail.ru>
2023-05-28 19:37:34 +0300

    KS-11-1 implemented task 1 for lesson 11

---
+++ src/main/kotlin/lesson11/User.kt
@@ -0,0 +1,12 @@
+package lesson11
+
+import sun.security.util.Password
+
+class User(
+    val id: Int,
+    val login: String,
+    val password: String,
+    val mail: String,
+) {
+
+}
---
+++ src/main/kotlin/lesson11/lesson_11_task_1.kt
@@ -0,0 +1,28 @@
+package lesson11
+
+fun main() {
+    val user1 = User(
+        id = 1,
+        login = "andrey1990@gmail.ru",
+        password = "123andrey",
+        mail = "andrey1990@gmail.ru",
+    )
+
+    val user2 = User(
+        id = 2,
+        login = "elena1995@gmail.ru",
+        password = "1995name",
+        mail = "elena1995@gmail.ru",
+    )
+
+    println(user1.id)
+    println(user1.login)
+    println(user1.password)
+    println(user1.mail)
+    println()
+    println(user2.id)
+    println(user2.login)
+    println(user2.password)
+    println(user2.mail)
+
+}
