726727faab22a761be7a5d635340c9a8d5b5a37d
Nata <nata.mir.99@mail.ru>
2023-05-29 16:07:32 +0300

    KS-11-2 implemented task 2 for lesson 11

--- src/main/kotlin/lesson11/lesson_11_task_2.kt
+++ src/main/kotlin/lesson11/lesson_11_task_2.kt
@@ -1,17 +1,55 @@
 package lesson11
 
 fun main() {
-val user1 = User (
-    id = 1,
-    login = "andrey1990@gmail.ru",
-    password = "123andrey",
-    mail = "andrey1990@gmail.ru",
-    bio = "",
-)
- user1.displayingUserInformation()
+    val user1 = User(
+        id = 1,
+        login = "andrey1990@gmail.ru",
+        password = "123andrey",
+        mail = "andrey1990@gmail.ru",
+        bio = "",
+    )
+    user1.displayingUserInformation()
     println("Введите инфомацию о себе:")
     user1.readingInformationAboutUsers()
     user1.changePassword()
-    user1.sendAMessage(text = user1.mail )
+    user1.sendAMessage(text = user1.mail)
 
+
+}
+
+
+class User(
+    val id: Int,
+    val login: String,
+    var password: String,
+    val mail: String,
+    var bio: String?,
+) {
+    fun displayingUserInformation() {
+        println(id)
+        println(login)
+        println(password)
+        println(mail)
+        println(bio)
+    }
+
+    fun readingInformationAboutUsers() {
+        bio = readln()
+    }
+
+    fun changePassword() {
+        println("Введите пароль:")
+        val userPassword = readln()
+        if (password == userPassword) {
+            println("Введите новый пароль:")
+            val userPasswordChanged = readln()
+            password = userPasswordChanged
+            println("Пароль изменен, Ваш новый пароль: $password")
+        }
+    }
+
+    fun sendAMessage(text: String) {
+        println(text)
+
+    }
 }
