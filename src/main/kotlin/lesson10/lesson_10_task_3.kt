90238f28670092e4195962e0923abc544bb40f01
Nata <nata.mir.99@mail.ru>
2023-05-29 15:42:48 +0300

    KS-10-3 implemented task 3 for lesson 10

--- src/main/kotlin/lesson10/lesson_10_task_3.kt
+++ src/main/kotlin/lesson10/lesson_10_task_3.kt
@@ -3,12 +3,12 @@ package lesson10
 fun main() {
     println("Введите количество символов:")
     val passwordLength = readln().toInt()
-    val randomString = steamGeneration(passwordLength)
+    val randomString = generatePassword(passwordLength)
     println(randomString)
 
 }
 
-fun steamGeneration(length: Int): String {
+fun generatePassword(length: Int): String {
     val charset = "0123456789 !\"#\$%&'()*+,-./"
     return (1..length)
         .map { charset.random() }
