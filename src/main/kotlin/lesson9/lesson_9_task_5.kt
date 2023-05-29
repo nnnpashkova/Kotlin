27c6f7d35debc02af77f925c2032852579ee9a8b
Nata <nata.mir.99@mail.ru>
2023-05-29 15:13:24 +0300

    KS-11-5 implemented task 5 for lesson 11

--- src/main/kotlin/lesson9/lesson_9_task_5.kt
+++ src/main/kotlin/lesson9/lesson_9_task_5.kt
@@ -4,14 +4,20 @@ import java.util.*
 
 fun main() {
     val recipes = mutableSetOf<String>()
-    while (recipes.size != 5){
+    while (recipes.size != 5) {
         println("Введите ингридиент ${recipes.size + 1}:")
         val userIngredient = readln()
         recipes.add(userIngredient)
     }
-    val result = "${recipes.sorted().joinToString()
-        .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }}."
-    println(result)
+    val sortedRecipes = recipes.sorted().toMutableList()
+    val lastElement = sortedRecipes.last()
+    sortedRecipes[sortedRecipes.lastIndex] = "$lastElement."
+
+    val sortingString = sortedRecipes.joinToString()
+    val capitalizedOutput = sortingString.
+    replaceFirstChar{ if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
+
+    println(capitalizedOutput)
 }
 
 
