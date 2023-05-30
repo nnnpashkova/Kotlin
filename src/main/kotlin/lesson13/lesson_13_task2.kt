abae7611dd881ede0b2bd4039a00f4ed7990fe93
Nata <nata.mir.99@mail.ru>
2023-05-30 19:56:24 +0300

    KS-13-2 implemented task 2 for lesson 13

--- src/main/kotlin/lesson13/lesson_13_task2.kt
+++ src/main/kotlin/lesson13/lesson_13_task2.kt
@@ -4,9 +4,8 @@ fun main() {
     val phonebook = Phonebook(
         name = "Сергей",
         phoneNumber = 89185140494,
-        company = null
+        company = null,
     )
-
     phonebook.printPhonebook()
 }
 
@@ -16,7 +15,7 @@ private class Phonebook(
     val company: String?,
 ) {
     fun printPhonebook() {
-        val companyNull: String = company?: "не указано"
+        val companyNull: String = company ?: "не указано"
         println("Имя: $name")
         println("Номер телефона: $phoneNumber")
         println("Компания: $companyNull")
