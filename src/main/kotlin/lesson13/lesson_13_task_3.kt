088b02971176687954872a8b498989c8938533f6
Nata <nata.mir.99@mail.ru>
2023-05-30 21:08:51 +0300

    KS-13-3 implemented task 3 for lesson 13

--- src/main/kotlin/lesson13/lesson_13_task_3.kt
+++ src/main/kotlin/lesson13/lesson_13_task_3.kt
@@ -1,47 +1,46 @@
 package lesson13
 
-    fun main() {
-        val phonebook = Phonebook(
-           name = "Сергей",
-            phoneNumber = 89185140494,
-            company = null
-        )
+fun main() {
+    val phonebook = Phonebook(
+        name = "Сергей",
+        phoneNumber = 89185140494,
+        company = null
+    )
 
-        val phonebook1 = Phonebook(
-            name = "Игорь",
-            phoneNumber = null,
-            company = "ОАО РЖД"
-        )
+    val phonebook1 = Phonebook(
+        name = "Игорь",
+        phoneNumber = null,
+        company = "ОАО РЖД"
+    )
 
-        val phonebook2 = Phonebook(
-            name = null,
-            phoneNumber = 89885041846,
-            company = "ОАО ЮГ СТРОЙ"
-        )
+    val phonebook2 = Phonebook(
+        name = null,
+        phoneNumber = 89885041846,
+        company = "ОАО ЮГ СТРОЙ"
+    )
 
-        //val contactList= listOf<Phonebook>( phonebook, phonebook1, phonebook2)
-
+    val contactList = listOf(phonebook, phonebook1, phonebook2)
+    contactList.forEach { phonebook ->
         phonebook.printPhonebook()
         println()
-        phonebook1.printPhonebook()
-        println()
-        phonebook2.printPhonebook()
     }
 
-    private class Phonebook(
-        val name: String?,
-        val phoneNumber: Long?,
-        val company: String?,
-    ) {
-        fun printPhonebook() {
-            val companyNull: String = company?: "не указано"
-            val phoneNumberNull: Comparable<*> = phoneNumber?: "не указано"
-            val nameNull: String = name?: "не указано"
-            println("Имя: $nameNull")
-            println("Номер телефона: $phoneNumberNull")
-            println("Компания: $companyNull")
-        }
+}
+
+private class Phonebook(
+    val name: String?,
+    val phoneNumber: Long?,
+    val company: String?,
+) {
+    fun printPhonebook() {
+        val companyNull: String = company ?: "не указано"
+        val phoneNumberNull: Comparable<*> = phoneNumber ?: "не указано"
+        val nameNull: String = name ?: "не указано"
+        println("Имя: $nameNull")
+        println("Номер телефона: $phoneNumberNull")
+        println("Компания: $companyNull")
     }
+}
 
 
 
