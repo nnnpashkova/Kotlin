cedd279455e30dc35fc3d3115b97546a5017b91e
Nata <nata.mir.99@mail.ru>
2023-05-30 21:10:17 +0300

    KS-13-3 implemented task 3 for lesson 13 - fix

--- src/main/kotlin/lesson13/lesson_13_task_3.kt
+++ src/main/kotlin/lesson13/lesson_13_task_3.kt
@@ -4,19 +4,19 @@ fun main() {
     val phonebook = Phonebook(
         name = "Сергей",
         phoneNumber = 89185140494,
-        company = null
+        company = null,
     )
 
     val phonebook1 = Phonebook(
         name = "Игорь",
         phoneNumber = null,
-        company = "ОАО РЖД"
+        company = "ОАО РЖД",
     )
 
     val phonebook2 = Phonebook(
         name = null,
         phoneNumber = 89885041846,
-        company = "ОАО ЮГ СТРОЙ"
+        company = "ОАО ЮГ СТРОЙ",
     )
 
     val contactList = listOf(phonebook, phonebook1, phonebook2)
