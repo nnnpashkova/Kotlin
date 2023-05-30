75daaefffa1596f6bfd44908838c9461cc3a4695
Nata <nata.mir.99@mail.ru>
2023-05-30 18:52:36 +0300

    KS-12-1 implemented task 1 for lesson 12 - fix

--- src/main/kotlin/lesson12/lesson_12_task_1.kt
+++ src/main/kotlin/lesson12/lesson_12_task_1.kt
@@ -2,34 +2,33 @@ package lesson12
 
 fun main() {
 val weather1 = Weather()
-    weather1.daytimeWeather = "+25"
-    weather1.weatherNight = "+15"
-    weather1.rain = false
-    weather1.atmospherePressure = "757мм"
+    weather1.daytimeWeather = 25
+    weather1.weatherNight = 15
+    weather1.isRaining = false
+    weather1.atmospherePressure = 757
 
     val weather2 = Weather()
-    weather2.daytimeWeather = "-10"
-    weather2.weatherNight = "-20"
-    weather2.rain = true
-    weather2.atmospherePressure = "770мм"
+    weather2.daytimeWeather = 10
+    weather2.weatherNight = 20
+    weather2.isRaining = true
+    weather2.atmospherePressure = 770
 
     weather1.print()
     println()
     weather2.print()
 }
 
+class Weather {
+    var daytimeWeather = 20
+    var weatherNight = 0
+    var isRaining = false
+    var atmospherePressure = 759
 
-
-class Weather() {
-    var daytimeWeather = "+20"
-    var weatherNight = "0"
-    var rain = false
-    var atmospherePressure = "759мм"
 
     fun print() {
-        println(daytimeWeather)
-        println(weatherNight)
-        println(rain)
-        println(atmospherePressure)
+        println("Погода днем: $daytimeWeather")
+        println("Погода ночью: $weatherNight")
+        println("Есть ли сегодня дождь: $isRaining")
+        println("Атмосферное давление: $atmospherePressure")
     }
 }
