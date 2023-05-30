86e74879f847b762dbbba7b47c5ab55ca084dd1e
Nata <nata.mir.99@mail.ru>
2023-05-30 22:47:35 +0300

    KS-13-4 implemented task 4 for lesson 13 - fix

--- src/main/kotlin/lesson13/lesson_13_task_4.kt
+++ src/main/kotlin/lesson13/lesson_13_task_4.kt
@@ -3,23 +3,48 @@ package lesson13
 fun main() {
     addingAnEntryToThePhonebook()
 }
+private class Phonebook(
+    val name: String?,
+    val phoneNumber: Long?,
+    val company: String?,
+) {
+    fun printPhonebook() {
+        println("Имя: $name")
+        println("Номер телефона: $phoneNumber")
+        println("Компания: $company")
+    }
+}
 
 fun addingAnEntryToThePhonebook() {
     do {
         println("Введите имя:")
-        val nameUser: String? = readln()
-        val phoneNumber = addingPhoneNumber()
+        val nameUser: String? = readln() ?: null
+        val phoneNumber: Long = addingPhoneNumber()
         println("Введите компанию:")
-        val companyUser: String? = readln()
+        val companyUser: String = readln()
+        val companyUserNull = if (companyUser.isEmpty()) {
+            null
+        } else {
+            companyUser
+        }
+
         println("Если хотите добавить новую запись, введите \\“да\\””")
-        val addingNumber: String? = readln()
+        val addingNumber: String = readln()
+
+        val phonebook = Phonebook(
+            name = nameUser,
+            phoneNumber = phoneNumber,
+            company = companyUserNull,
+        )
+        phonebook.printPhonebook()
+
     } while (addingNumber == "да")
 }
 
-fun addingPhoneNumber(): Int {
+fun addingPhoneNumber(): Long {
     while (true) {
         println("Введите номер телефона:")
-        val phoneUser: Int? = readln().toInt()
+        val phoneUser: Long? = readln().toLong()
         if (phoneUser != null) {
             return phoneUser
         }
