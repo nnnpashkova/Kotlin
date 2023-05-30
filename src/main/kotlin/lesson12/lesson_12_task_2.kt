e87c8be14969a8b798e4ada28db54646a2751bd3
Nata <nata.mir.99@mail.ru>
2023-05-30 19:33:55 +0300

    KS-12-2 implemented task 2 for lesson 12 - fix

--- src/main/kotlin/lesson12/lesson_12_task_2.kt
+++ src/main/kotlin/lesson12/lesson_12_task_2.kt
@@ -1,36 +1,49 @@
 package lesson12
 
 fun main() {
-    val weather1 = WeatherForecast(
-        daytimeWeather = "+25",
-        weatherNight = "+15",
-        rain = false,
-        atmospherePressure = "757мм"
-    )
+    val weatherForecast1 = WeatherForecast()
+    weatherForecast1.daytimeWeather = 25
+    weatherForecast1.weatherNight = 15
+    weatherForecast1.rain = false
+    weatherForecast1.atmospherePressure = 757
+    weatherForecast1.airTemperature = "+"
+    weatherForecast1.measure = "мм"
 
+    val weatherForecast2 = WeatherForecast()
+    weatherForecast2.daytimeWeather = 2
+    weatherForecast2.weatherNight = 10
+    weatherForecast2.rain = true
+    weatherForecast2.atmospherePressure = 700
+    weatherForecast2.airTemperature = "-"
+    weatherForecast2.measure = "мм"
 
-    val weather2 = WeatherForecast(
-        daytimeWeather = "-10",
-        weatherNight = "-20",
-        rain = true,
-        atmospherePressure = "770мм"
-    )
+    val weatherForecast3 = WeatherForecast()
+    weatherForecast3.daytimeWeather = 33
+    weatherForecast3.weatherNight = 29
+    weatherForecast3.rain = false
+    weatherForecast3.atmospherePressure = 770
+    weatherForecast3.airTemperature = "+"
+    weatherForecast3.measure = "мм"
 
-    weather1.print()
+    weatherForecast1.print()
     println()
-    weather2.print()
+    weatherForecast2.print()
+    println()
+    weatherForecast3.print()
 }
 
-class WeatherForecast(
-    val daytimeWeather: String,
-    val weatherNight: String,
-    val rain: Boolean,
-    val atmospherePressure: String,
-) {
+class WeatherForecast {
+    var daytimeWeather = 25
+    var weatherNight = 15
+    var rain = false
+    var atmospherePressure = 757
+    var airTemperature = "+"
+    var measure = "мм"
     fun print() {
-        println(daytimeWeather)
-        println(weatherNight)
-        println(rain)
-        println(atmospherePressure)
+        println("Температура днем: $airTemperature$daytimeWeather")
+        println("Температура ночью: $airTemperature$weatherNight")
+        println("Есть ли дождь: $rain")
+        println("Атмосферное давление: $atmospherePressure$measure")
+
     }
 }
