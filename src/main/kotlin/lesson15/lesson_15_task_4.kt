002fe227b0ec36c13d3f46209a553857b2b49e84
Nata <nata.mir.99@mail.ru>
2023-06-05 19:11:14 +0300

    KS-15-4 implemented task 4 for lesson 15

---
+++ src/main/kotlin/lesson15/lesson_15_task_4.kt
@@ -0,0 +1,48 @@
+package lesson15
+
+fun main() {
+    val tools = Tools(
+        "гитара",
+        5
+    )
+
+    val accessories = Accessories(
+        "струны", 10
+    )
+    tools.search()
+    accessories.search()
+}
+
+abstract class Product(
+    val name: String,
+    val quantity: Int
+) {
+
+}
+
+interface ProductSearch {
+    fun search()
+}
+
+class Tools(
+    name: String,
+    quantity: Int
+) : Product(name, quantity), ProductSearch {
+    override fun search() {
+        println("Выполняется поиск, категория: инструменты, название: $name")
+    }
+}
+
+
+class Accessories(
+    name: String,
+    quantity: Int
+) : Product(name, quantity), ProductSearch {
+    override fun search() {
+        println("Выполняется поиск, категория: комплектующие, название: $name")
+    }
+}
+
+
+
+
