edae0a5dd99151d2b5ffea31a34a27de84f3d11b
Nata <nata.mir.99@mail.ru>
2023-06-07 14:12:58 +0300

    KS-17-4 implemented task 4 for lesson 17 - fix

--- src/main/kotlin/lesson17/lesson_17_task_4.kt
+++ src/main/kotlin/lesson17/lesson_17_task_4.kt
@@ -1,16 +1,16 @@
 package lesson17
 
 fun main() {
-    val package1 = Package()
+    val package1 = Package(2356, "Краснодар")
     package1.location = "Кисловодск"
     println("Местоположение поссылки: ${package1.location}")
     println("${package1.displacementCounter}")
 }
 
-class Package() {
-    private val number: Int = 2356
+class Package(number: Int, location: String,) {
+    private val number = number
     var displacementCounter = 0
-    var location: String = "Калининград"
+    var location: String = location
         set(value) {
            displacementCounter++
             field = value
