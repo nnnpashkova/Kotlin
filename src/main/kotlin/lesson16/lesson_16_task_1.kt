84b416ad377d145df756054001f16150eef53be7
Nata <nata.mir.99@mail.ru>
2023-06-07 12:11:09 +0300

    KS-16-1 implemented task 1 for lesson 16 - fix

--- src/main/kotlin/lesson16/lesson_16_task_1.kt
+++ src/main/kotlin/lesson16/lesson_16_task_1.kt
@@ -2,17 +2,16 @@ package lesson16
 
 fun main() {
     var gameCude = GameCube(
-        namder = (1..6).random()
+        number = (1..6).random()
     )
     gameCude.showNumber()
-
 }
 
 class GameCube(
-    private val namder:Int
-){
-   fun showNumber() {
-       println("Число от 1 до 6: $namder")
-   }
+    private val number: Int
+) {
+    fun showNumber() {
+        println("Число от 1 до 6: $number")
+    }
 
 }
