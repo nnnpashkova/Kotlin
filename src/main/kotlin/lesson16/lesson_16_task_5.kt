a7f24d59b8335a773553e5cc4473de8327fd02c3
Nata <nata.mir.99@mail.ru>
2023-06-07 11:59:28 +0300

    KS-15-5 implemented task 5 for lesson 15

--- src/main/kotlin/lesson16/lesson_16_task_5.kt
+++ src/main/kotlin/lesson16/lesson_16_task_5.kt
@@ -5,19 +5,4 @@ fun main()
 
 }
 
-class Player(
-    val name: String,
-    val health: String,
-    val impactForce: String,
-){
-    fun damageReceived(){
-
-    }
-    fun discern() {
-
-    }
-    private fun death() {
-
-    }
-
-}
+c
