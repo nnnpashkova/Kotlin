6567472e67df951c986f091a3c29a4f7051aeff6
Nata <nata.mir.99@mail.ru>
2023-06-07 13:08:26 +0300

    KS-17-2 implemented task 2 for lesson 17 - fix

--- src/main/kotlin/lesson17/lesson_17_task_2.kt
+++ src/main/kotlin/lesson17/lesson_17_task_2.kt
@@ -2,19 +2,16 @@ package lesson17
 
 fun main() {
     val ship = Ship()
-    ship.rewriteError = "Фаил перезаписать нельзя"
     ship.name = "Якорь"
     println("Название: ${ship.name} \nСкорость: ${ship.averageSpeed} узлов, \nПорт приписки: ${ship.port}")
 
 }
 
 class Ship {
-    var rewriteError = ""
     var name = "Алмаз"
-     get() = field
-    set(value) {
-      field = rewriteError
-    }
+        set(value) {
+            println("Фаил перезаписать нельзя")
+        }
 
     val averageSpeed = 40
     val port: String = "Алания"
