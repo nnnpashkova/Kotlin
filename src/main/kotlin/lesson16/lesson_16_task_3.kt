6c8e20450ecb3bd9dd7a147ea86f48829104d0d8
Nata <nata.mir.99@mail.ru>
2023-06-07 13:13:18 +0300

    KS-16-3 implemented task 3 for lesson 16 - fix

--- src/main/kotlin/lesson16/lesson_16_task_3.kt
+++ src/main/kotlin/lesson16/lesson_16_task_3.kt
@@ -7,26 +7,21 @@ fun main() {
         "list@mail.ru",
         "123456List"
     )
-    userInterface.checkPassword()
-
+    println("Введите пароль:")
+    val userPassword = readln()
+    userInterface.checkPassword(userPassword)
 }
 
 class UserInterface(
     val login: String,
     private val password: String
 ) {
-    fun checkPassword() {
-        println("Введите логин:")
-        val userLogin = readln()
-        println("Введите пароль:")
-        do {
-            val userPassword = readln()
-            if (password == userPassword) {
-                println("Пароль введен верно")
-            } else {
-                println("Введите пароль заново:")
-            }
-        } while (password != userPassword)
+    fun checkPassword(userPassword:String) {
+        if (password == userPassword) {
+            println("Пароль введен верно")
+        } else {
+            println("Пароль введен неверно")
+        }
     }
 }
 
