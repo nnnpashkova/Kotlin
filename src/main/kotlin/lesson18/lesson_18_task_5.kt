1b92aee8deca0c97201b0eb0899e12802ddd1753
Nata <nata.mir.99@mail.ru>
2023-06-09 22:02:09 +0300

    KS-18-5 implemented task 5 for lesson 18

---
+++ src/main/kotlin/lesson18/lesson_18_task_5.kt
@@ -0,0 +1,27 @@
+package lesson18
+
+fun main() {
+
+}
+
+
+
+class Dot(
+    x: Int,
+    y: Int
+)  {
+
+}
+
+class DotF(
+    x: Float,
+    y: Float
+) {
+
+}
+interface Figura
+
+class Сircle (
+    х:Int,
+    y
+)
