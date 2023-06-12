ee58357deee43801526839b00137432badece09a
Nata <nata.mir.99@mail.ru>
2023-06-12 22:32:04 +0300

    KS-19-3 implemented task 3 for lesson 19

--- src/main/kotlin/lesson19/lesson_19_task_3.kt
+++ src/main/kotlin/lesson19/lesson_19_task_3.kt
@@ -1,5 +1,21 @@
 package lesson19
 
 fun main() {
+    val spaceship = Spaceship(true, true, true)
+    println(spaceship.takeOffTheShip())
+    println(spaceship.landTheShip())
+}
 
+class Spaceship(
+    val canTakeOff: Boolean,
+    val canLand: Boolean,
+    val canShootBack: Boolean,
+) {
+    fun takeOffTheShip() {
+        //TODO здесь нужна информация о дополнительной логике
+    }
+
+    fun landTheShip() {
+        TODO("NotImplementedError")
+    }
 }
