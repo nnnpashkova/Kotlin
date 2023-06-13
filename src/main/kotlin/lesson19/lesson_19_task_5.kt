b38f92da0a2f2c010c2dfd6f9519c17acbcbd39a
Nata <nata.mir.99@mail.ru>
2023-06-13 21:35:56 +0300

    KS-19-5 implemented task 5 for lesson 19

---
+++ src/main/kotlin/lesson19/lesson_19_task_5.kt
@@ -0,0 +1,30 @@
+package lesson19
+
+fun main() {
+    val filingData = mutableListOf<Human>()
+    println("Введите имя с большой буквы, укажите пол: мужской или женский")
+    for (i in 0 until 5) {
+        val userName = readln()
+        val userGender = readln()
+        filingData.add(
+            Human(
+                name = userName, gender = if (userGender == "женский") {
+                    Gender.FEMALE
+                } else {
+                    Gender.MALE
+                }
+            )
+        )
+    }
+    filingData.forEach { println(it) }
+}
+
+data class Human(
+    val name: String,
+    val gender: Gender,
+)
+
+enum class Gender {
+    MALE,
+    FEMALE,
+}
