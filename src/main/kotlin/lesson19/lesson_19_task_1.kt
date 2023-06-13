00681fe565dc78fd3d75bfe8d304214f97d74cdd
Nata <nata.mir.99@mail.ru>
2023-06-13 19:02:12 +0300

    KS-19-1 implemented task 1 for lesson 19

--- src/main/kotlin/lesson19/lesson_19_task_1.kt
+++ src/main/kotlin/lesson19/lesson_19_task_1.kt
@@ -4,20 +4,15 @@ fun main() {
     for (i in Fish.values()) informUser(i)
 }
 
-enum class Fish {
-    GUPPY,
-    ANGELFISH,
-    GOLDFISH,
-    SIAMESE_FIGHTING_FISH,
+enum class Fish(val fishName: String) {
+    GUPPY("Гуппи"),
+    ANGELFISH("Cкалярия"),
+    GOLDFISH("Золотая рыбка"),
+    SIAMESE_FIGHTING_FISH("Петушок"),
 }
 
 fun informUser(fish: Fish) {
-    when (fish) {
-        Fish.GUPPY -> println("Гуппи")
-        Fish.ANGELFISH -> println("Cкалярия")
-        Fish.GOLDFISH -> println("Золотая рыбка")
-        Fish.SIAMESE_FIGHTING_FISH -> println("Петушок")
-    }
+    println(fish.fishName)
 }
 
 
