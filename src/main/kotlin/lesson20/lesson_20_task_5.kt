20e8ffd3618818c6d5aa7e8c6ea5bb9f1c42bc98
Nata <nata.mir.99@mail.ru>
2023-06-14 14:09:02 +0300

    KS-20-5 implemented task 5 for lesson 20

---
+++ src/main/kotlin/lesson20/lesson_20_task_5.kt
@@ -0,0 +1,13 @@
+package lesson20
+
+fun main() {
+
+}
+
+enum class Robot(robotPhrases: String){
+    FIRST_PHRASE("Привет"),
+    SECOND_PHRASE("Как прошел твой день?"),
+    THIRD_PHRASE("Сегодня солнечный день"),
+    FOURTH_PHRASE("Хорошего дня!"),
+    FIFTH_PHRASE("Улыбайся!")
+}
