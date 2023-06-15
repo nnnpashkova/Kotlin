56472826fbf993e822ce73ec014f488dcf5f220a
Nata <nata.mir.99@mail.ru>
2023-06-15 14:41:04 +0300

    KS-16-5 implemented task 5 for lesson 16

--- src/main/kotlin/lesson16/lesson_16_task_5.kt
+++ src/main/kotlin/lesson16/lesson_16_task_5.kt
@@ -1,8 +1,37 @@
 package lesson16
 
-fun main()
-{
-
+fun main() {
+    var game = Gamer("Александр", 100, 30)
+    game.damage(100)
+    game.discern(10)
 }
 
-c
+class Gamer(
+    val name: String,
+    private var health: Int,
+    var impactForce: Int
+) {
+
+    fun damage(damage: Int) {
+        health -= damage
+        if (health <= 0) {
+            death()
+            impactForce = 0
+        } else {
+            println("Вы получили удар, ваше здоровье стало $health")
+        }
+    }
+
+    fun discern(treatment: Int) {
+        if (health <= 0) {
+            println("Вас вылечить невозможно!")
+        } else {
+            health += treatment
+            println("Ваше здоровье после лечения $health")
+        }
+    }
+
+    private fun death() {
+        println("Ваше здоровье: $health, Вы умерли")
+    }
+}
