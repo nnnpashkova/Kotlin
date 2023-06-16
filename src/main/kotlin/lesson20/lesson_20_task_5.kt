6172dc0d21fcfa48e659e74c04dbb5eb4465a5c7
Nata <nata.mir.99@mail.ru>
2023-06-16 21:52:26 +0300

    KS-20-5 implemented task 5 for lesson 20

--- src/main/kotlin/lesson20/lesson_20_task_5.kt
+++ src/main/kotlin/lesson20/lesson_20_task_5.kt
@@ -1,13 +1,22 @@
 package lesson20
 
 fun main() {
-
+    val phrase = listOf("Привет", "Как прошел твой день?", "Сегодня солнечный день", "Хорошего дня!", "Улыбайся!")
+    val robot = Robot()
+    phrase.forEach { robot.say(it) }
+    println("Сейчас слова будут наоборот:")
+    robot.setModifier { it.reversed() }
+    phrase.forEach { robot.say(it) }
 }
 
-enum class Robot(robotPhrases: String){
-    FIRST_PHRASE("Привет"),
-    SECOND_PHRASE("Как прошел твой день?"),
-    THIRD_PHRASE("Сегодня солнечный день"),
-    FOURTH_PHRASE("Хорошего дня!"),
-    FIFTH_PHRASE("Улыбайся!")
+class Robot {
+    private var modifier = { phrase: String -> phrase }
+
+    fun say(phrase: String) {
+        println(modifier(phrase))
+    }
+
+    fun setModifier(modifier: (String) -> String) {
+        this.modifier = modifier
+    }
 }
