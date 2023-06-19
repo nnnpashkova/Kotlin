e92f7e8e840155f8ea66fcafd96e01e78e43c860
Nata <nata.mir.99@mail.ru>
2023-06-19 20:47:34 +0300

    KS-11-5 implemented task 5 for lesson 11

--- src/main/kotlin/lesson11/lesson_11_task_5.kt
+++ src/main/kotlin/lesson11/lesson_11_task_5.kt
@@ -2,34 +2,44 @@ package lesson11
 
 fun main() {
     val forum = Forum()
-
+    val user1 = forum.newUser("sofa12345", "ghgh9090", "sofa@mail.ru")
+    val user2 = forum.newUser("sofa12345", "ghgh9090", "sofa@mail.ru")
+    val id1 = user1.id
+    val id2 = user2.id
+    forum.newMessage(id1, "Привет, пользователь приложения!")
+    forum.newMessage(id2, "Привет")
+    forum.printThread()
 }
 
 class Forum(
-    private val user: MutableList<User>
+    private val userList: MutableList<User> = mutableListOf(),
+    private var lastUserId: Int = 0,
 ) {
-    fun newUser() {
-        val user =
-       User.newUser()
+    private val userMessages: MutableMap<Int, MutableList<String>> = mutableMapOf()
+    fun newUser(login: String, password: String, mail: String): User {
+        lastUserId++
+        val user = User(lastUserId, login, password, mail)
+        userList.add(user)
+        return user
     }
 
-    fun newMessage(id: Int, messages: String) {
+    fun newMessage(id: Int, message: String) {
+        val messages = userMessages[id]
+        if (messages != null) {
+            messages.add(message)
+        } else {
+            userMessages[id] = mutableListOf(message)
+        }
+    }
 
+    fun printThread() {
+        userMessages.forEach { println("Пользоватль ${it.key}, сообщение: ${it.value}") }
     }
 }
 
-class User private constructor(
+class User(
     val id: Int,
     val login: String,
     var password: String,
     val mail: String,
-    // var bio: String?,
-) {
-    companion object {
-        fun newUser(): User {
-            val user = User(334, "andrey1990", "123andrey", "andrey1990@gmail.ru")
-            return user
-        }
-    }
-
-}
+)
