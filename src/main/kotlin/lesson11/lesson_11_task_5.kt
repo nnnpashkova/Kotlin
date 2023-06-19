47fc01a83513a4b0aa485026adc701bc3e4cebd2
Nata <nata.mir.99@mail.ru>
2023-06-19 13:02:43 +0300

    KS-11-5 implemented task 5 for lesson 11

--- src/main/kotlin/lesson11/lesson_11_task_5.kt
+++ src/main/kotlin/lesson11/lesson_11_task_5.kt
@@ -1,14 +1,35 @@
 package lesson11
 
 fun main() {
+    val forum = Forum()
 
 }
-class User(
+
+class Forum(
+    private val user: MutableList<User>
+) {
+    fun newUser() {
+        val user =
+       User.newUser()
+    }
+
+    fun newMessage(id: Int, messages: String) {
+
+    }
+}
+
+class User private constructor(
     val id: Int,
     val login: String,
     var password: String,
     val mail: String,
-    var bio: String?,
-){
+    // var bio: String?,
+) {
+    companion object {
+        fun newUser(): User {
+            val user = User(334, "andrey1990", "123andrey", "andrey1990@gmail.ru")
+            return user
+        }
+    }
 
 }
